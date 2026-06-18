a6c32f1296af1edc22403f2dcf5503240e026c8b
Arnaudguetch <arnaud.guetch@gmail.com>
2026-06-18 04:32:38 +0200

    Feature/fix issues (#143)
    * push docker image to acr
    
    * allow kubernetes to pull image from acr
    
    * updated modified:   k8s/deployment.yaml
    
    * modified:   .github/workflows/ci-cd-minikube.yml
    
    * fix kubernetes port-forward service port
    
    * add ingress config
    
    * fix workflows/ci-cd-minikube.yml and readme
    
    * fix bug to deploy
    
    * Udapted readme
    
    * add streamlit deployment
    
    * fix streamlit deployment yaml indentation
    
    * Update ingress
    
    * fix streamlit api url in kubernetes
    
    * New files mlflow to the VM
    
    * fix bug mlflow-service
    
    * fix bug ci-cd-minikube
    
    * fix bug src/train and workflow
    
    * fix bugs ++
    
    * add other
    
    * Modify
    
    * fix some burgs
    
    * fix burg in train model into the workflow
    
    * fix burg
    
    * modify mlflom-deploy
    
    * Add words to readme
    
    * fix burg in deployment
    
    * fix burg ci-cd
    
    * fix and modify some burgs
    
    * buuuuuuuuuuuuuuuurrrrrrrrgggg
    
    * r========================t
    
    * someeeeeeeee burgs
    
    * ci-cd-minikube.yml
    
    * fix burgs train and ci-cd
    
    * repair some burgs on fastapi
    
    * updated readme-------------------
    
    * redeploy after minikube reset
    
    * fix mflow service and ingress
    
    * Updated streamlit ready for the prod
    
    * Tried to fix some bugs from mlflow
    
    * Fixed docker.mlflow
    
    * fixed dockerfile.mlflow again
    
    * modified:   k8s/mlflow-deployment.yaml
    
    * fixed k8s/mlflow-deployment.yaml
    
    * ........................
    
    * fixed again k8s/mlflow-deployment.yaml
    
    * Fixed=================k8s/mlflow-deployment.yaml
    
    * ..........................
    
    * =================================
    
    * fixed x1
    
    * fixed x2
    
    * modified:   k8s/deployment.yaml
    
    * modified:   k8s/mlflow-deployment.yaml
    
    * Fixed k8s/mlflow-deployment.yaml and src/train.py
    
    * ===================+++++++++++
    
    * mlflow-pvc.yaml
    
    * Refactored all files
    
    * Fixed .github/workflows/ci-cd-minikube.yml
    
    * Fixed ci-cd
    
    * Fixed again .github/workflows/ci-cd-minikube.yml
    
    * improved .github/workflows/ci-cd-minikube.yml
    
    * Changed word in ci-cd
    
    * Fixed ci-cd/k8s-1
    
    * fixed ci-cd/k8s_4
    
    * Fixed some essentials bugs
    
    * Fixed ci-cd-minikube.yml and secretproviderclass.yaml
    
    * fixed ImageName bugs
    
    * Fix non-root user for AKS
    
    * fixed image into streamlit ci-cd and deployment
    
    * Fixed mlflow's dockerfile and deployment
    
    * fixed some bugs on dockerfile and mlflow-deployment
    
    * Dockerfile.mlflow
    
    * Dockerfile.mlflow
    
    * fix mlflow startup
    
    * fixed bugs Dockerfile.mlflow
    
    * Fixed k8s/mlflow-deployment.yaml
    
    * Fixed Dockerfile.mlflow and k8s/mlflow-deployment.yaml
    
    * fixed buggggggggss
    
    * Version stable
    
    * Added Pydantic
    
    * Fixed issue Pydantic

--- api/app.py
+++ api/app.py
@@ -3,7 +3,25 @@ import joblib
 import pandas as pd
 from fastapi import FastAPI, HTTPException
 from contextlib import asynccontextmanager
+from pydantic import BaseModel, Field
 
+class CreditRequest(BaseModel):
+    Age: int
+    Sex: str
+    Job: int
+    Housing: str
+
+    Saving_accounts: str = Field(alias="Saving accounts")
+    Checking_account: str = Field(alias="Checking account")
+    Credit_amount: float = Field(alias="Credit amount")
+
+    Duration: int
+    Purpose: str
+
+    model_config = {
+        "populate_by_name": True
+    }
+    
 MODEL_PATH = os.getenv("MODEL_PATH", "models/xgboost_pipeline.pkl")
 
 model = None
@@ -14,9 +32,11 @@ async def lifespan(app: FastAPI):
     global model
     try:
         model = joblib.load(MODEL_PATH)
+        print("Model loaded successfully")
     except Exception as e:
         print(f"Model loading failed: {e}")
         model = None
+
     yield
 
 
@@ -40,12 +60,12 @@ def ready():
 
 
 @app.post("/predict")
-def predict(payload: dict):
+def predict(payload: CreditRequest):
     if model is None:
         raise HTTPException(status_code=503, detail="Model not loaded")
 
     try:
-        df = pd.DataFrame([payload])
+        df = pd.DataFrame([payload.model_dump(by_alias=True)]) 
         proba = model.predict_proba(df)[0][1]
 
         return {
